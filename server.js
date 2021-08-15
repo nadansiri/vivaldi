e89d4b1212826b820847ba2c14d6979bf6381355
nadansiri <nada19nsiri@gmail.com>
2021-08-16 00:27:38 +0100

    changements css

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ app.get("*", function (req, res) {
 });*/ 
 
   //For Heroku
-  const _dirname = path.resolve()
+  const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname,'client/build')));
+  app.use(express.static(path.join(__dirname,'client/build')));
   app.get("*", (req, res)=> {
-    res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   })
 } else{
   app.get("/", (req, res)=> {
