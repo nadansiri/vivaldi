11cd53c24efac40eb1746af08d18aefde0ab5619
nadansiri <nada19nsiri@gmail.com>
2021-08-15 03:28:34 +0100

    persisting redux1

--- client/src/Redux/store/store.js
+++ client/src/Redux/store/store.js
@@ -6,14 +6,32 @@ import thunk from "redux-thunk"
 //import rootReducer
 import rootReducer from "../reducers"
 
+
+function saveToLocalStorage(state) {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+}
+
+function loadFromLocalStorage() {
+const serializedState = localStorage.getItem('state');
+if (serializedState === null) return undefined;
+   return JSON.parse(serializedState);
+}
+
+
 //create composeEnhancer
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const presistedState = loadFromLocalStorage();
+
 
 //store
 
 const store = createStore(
-    rootReducer,
+    rootReducer,presistedState,
     composeEnhancer(applyMiddleware(thunk)) 
 )
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store 
