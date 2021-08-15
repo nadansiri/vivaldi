bc33ade42048a118aa27b4cfbde4f10fd68952ba
nadansiri <nada19nsiri@gmail.com>
2021-08-15 01:57:20 +0100

    deploy test1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 //require the db
-const connectDB = require("./config/connectDB");
+const connectDB = require("./config/connectDB"); 
  
 //routes
 //const userRoutes = require('./routes/api/userRoutes')
